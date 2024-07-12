9c9512554bb28f62435398d5b3ac5a21b2cc0d94
Elena Gorozheeva <prodatamax@yandex.ru>
2024-07-12 14:48:23 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const gallery = document.querySelector(".places__list");
+const template = document.querySelector("#card-template").content;
+
+//создать карточку
+function createCard(name, link) {
+  const card = template.cloneNode(true);
+  card.querySelector(".card__title").textContent = name;
+  card.querySelector(".card__image").src = link;
+  card
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+
+  return card;
+}
+
+//добавить карточку в галерею
+initialCards.forEach((card) => {
+  const newCard = createCard(card.name, card.link);
+  gallery.append(newCard);
+});
+
+// Функция для удаления карточки
+function deleteCard(event) {
+  const card = event.target.closest(".card");
+  card.remove();
+}
